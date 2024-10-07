a6b1554918eee0b5ea9606f684df034c40592bea
pierreyoda <pierreyoda@users.noreply.github.com>
2024-10-07 18:17:08 +0200

    :sparkles: Website: add simple error page

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="website-container flex items-center justify-center">
+  <h1 class="text-4xl p-16 text-gray-600">{$page.error?.message}</h1>
+</div>
