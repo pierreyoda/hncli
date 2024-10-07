d155aa4d49f9781ef77067f873d67207fd9c4594
pierreyoda <pierreyoda@users.noreply.github.com>
2024-10-07 18:16:50 +0200

    :lipstick: Website: add newstackwhodis blog link in header

--- website/src/lib/ExternalLink.svelte
+++ website/src/lib/ExternalLink.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   export let href: string;
   export let extraClass = "";
+  export let underline = true;
 </script>
 
-<a class={extraClass} {href} target="_blank" rel="noreferrer noopener"><slot /></a>
+<a class={extraClass} class:underline {href} target="_blank" rel="noreferrer noopener"><slot /></a>
 
 <style lang="postcss">
   a {
-    @apply text-hncli-dark-red underline transition-colors duration-200 hover:text-hncli-dark-red/60;
+    @apply text-hncli-dark-red transition-colors duration-200 hover:text-hncli-dark-red/60;
   }
 </style>
--- website/src/lib/Header.svelte
+++ website/src/lib/Header.svelte
@@ -1,29 +1,31 @@
 <script lang="ts">
+  import ExternalLink from "./ExternalLink.svelte";
+
   import GitHubLogo from "$lib/assets/github.svg";
   import HnCliTextLogo from "$lib/assets/hncli-logo-text.png";
 </script>
 
 <header class="website-container">
-  <div />
-  <nav class="mr-12 md:mr-0">
-    <a href="/" aria-label="Home" class="flex items-center">
+  <nav>
+    <a href="/" aria-label="Home" class="flex flex-col items-center">
       <img alt="Wrench icon" src={HnCliTextLogo} class="max-w-40 h-auto" />
       <h1 class="hidden">hncli</h1>
     </a>
   </nav>
-  <a href="https://github.com/pierreyoda/hncli" title="hncli GitHub repository">
-    <img alt="GitHub logo" src={GitHubLogo} class="w-10 h-10" />
-  </a>
+  <div class="flex items-center">
+    <ExternalLink
+      href="https://www.newstackwhodis.com/blog"
+      underline={false}
+      extraClass="mr-6 no-underline text-xl font-medium text-hncli-dark-red">Blog</ExternalLink
+    >
+    <ExternalLink href="https://github.com/pierreyoda/hncli" extraClass="cursor-pointer">
+      <img alt="GitHub logo" src={GitHubLogo} class="w-10 h-10" />
+    </ExternalLink>
+  </div>
 </header>
 
 <style lang="postcss">
   header {
-    @apply mx-0 flex items-center py-8 md:justify-between;
-  }
-
-  nav {
-    a {
-      @apply no-underline;
-    }
+    @apply flex items-center justify-between py-8 pl-10 md:pl-0;
   }
 </style>
