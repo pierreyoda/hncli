cbc200b4d92d114e45a269df02f39c26422daf54
pierreyoda <pierreyoda@users.noreply.github.com>
2025-07-03 19:56:06 +0200

    :memo: More HeroTitle content

--- website/src/lib/content/HeroTitle.svelte
+++ website/src/lib/content/HeroTitle.svelte
@@ -5,10 +5,19 @@
 <section id="hero-title" aria-label="hero section title">
   <h2 class="text-4xl font-semibold tracking-tight text-gray-800">
     Browse
-    <ExternalLink href="https://news.ycombinator.com/">Hacker News</ExternalLink>&nbsp;from your terminal.
+    <ExternalLink extraClass="underline" href="https://news.ycombinator.com/">Hacker News</ExternalLink>&nbsp;from your
+    terminal.
   </h2>
   <p class="mt-6 text-lg text-gray-600 font-medium">
     Convenient, fast, and will run well on most computers, even old ones.
+  </p>
+  <p class="text-xl font-medium tracking-tight text-gray-700 mt-4">
+    <ExternalLink
+      extraClass="no-underline mt-4"
+      href="https://www.newstackwhodis.com/blog/hncli-1-concept#an-hacker-news-terminal-ui-concept-and-road-to-release"
+    >
+      Concept, vision and UX</ExternalLink
+    >
   </p>
 </section>
 
