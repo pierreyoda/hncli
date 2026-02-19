39bd75521785c90bf5a3a5c7f0fd13ee33137bd7
pierreyoda <pierreyoda@users.noreply.github.com>
2026-02-19 14:45:57 +0100

    :lipstick: Add magenta theme

--- src/ui/theme.rs
+++ src/ui/theme.rs
@@ -1,13 +1,14 @@
 use ratatui::style::Color;
 use serde::{Deserialize, Serialize};
 
-/** Avoid boilerplate and easily add theme colors and palettes. */
+/// Avoid boilerplate and easily add theme colors and palettes.
 macro_rules! theme_define_color_palettes {
-    ($ ( $name: ident : $color_blue: expr, $color_yellow: expr , )* ) => {
+    ($ ( $name: ident : $color_blue: expr, $color_magenta: expr, $color_yellow: expr , )* ) => {
     $(
         pub fn $name(&self) -> Color {
             match self {
                 Self::Blue => $color_blue,
+                Self::Magenta => $color_magenta,
                 Self::Yellow => $color_yellow,
             }
         }
@@ -18,6 +19,7 @@ macro_rules! theme_define_color_palettes {
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub enum UiTheme {
     Blue,
+    Magenta,
     #[default]
     Yellow,
 }
@@ -25,7 +27,8 @@ pub enum UiTheme {
 impl UiTheme {
     pub fn next_value(&self) -> UiTheme {
         match self {
-            Self::Blue => Self::Yellow,
+            Self::Blue => Self::Magenta,
+            Self::Magenta => Self::Yellow,
             Self::Yellow => Self::Blue,
         }
     }
@@ -33,13 +36,14 @@ impl UiTheme {
     pub fn label(&self) -> &'static str {
         match self {
             Self::Blue => "Blue",
+            Self::Magenta => "Magenta",
             Self::Yellow => "Yellow",
         }
     }
 
     theme_define_color_palettes! {
-        get_main_color: Color::LightBlue, Color::LightYellow,
-        get_block_color: Color::LightCyan, Color::White,
-        get_accent_color: Color::LightMagenta, Color::Yellow,
+        get_main_color: Color::LightBlue, Color::LightMagenta, Color::LightYellow,
+        get_block_color: Color::LightCyan, Color::White, Color::White,
+        get_accent_color: Color::LightMagenta, Color::Magenta, Color::Yellow,
     }
 }
