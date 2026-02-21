ef213ac1b30aedfd59f15de2e16b476925d34399
pierreyoda <pierreyoda@users.noreply.github.com>
2026-02-21 13:50:35 +0100

    :lipstick: Website: improved footer design, sticky footer on error page

--- website/src/lib/Footer.svelte
+++ website/src/lib/Footer.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   const currentYear = new Date().getFullYear();
-  // TODO: sticky bottom footer
 </script>
 
 <footer>
-  <div class="text-center flex flex-col justify-center items-center w-full h-full">
+  <div class="text-center flex flex-col justify-center items-center w-full">
     <p class="text">
       &copy; Copyright {currentYear} Pierre-Yves Diallo
     </p>
@@ -15,10 +14,10 @@
   @reference "../app.css";
 
   footer {
-    @apply border-hncli-light-orange/50 border-t-4 bg-gray-900;
+    @apply flex flex-col items-center bg-gray-900;
   }
 
   .text {
-    @apply py-4 text-base font-medium tracking-tight text-white;
+    @apply py-4 font-medium text-white;
   }
 </style>
